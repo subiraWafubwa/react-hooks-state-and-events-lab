c20307064cdc26b1b05ca3bbe3efab146e3cadab
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-08-08 22:39:43 +0300

    Passed Tests

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleSelectedCategory = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const filteredItems = items.filter((item) => {
+    return (
+      selectedCategory === "" ||
+      selectedCategory === "All" ||
+      item.category === selectedCategory
+    );
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select
+          name="filter"
+          value={selectedCategory}
+          onChange={handleSelectedCategory}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +31,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
